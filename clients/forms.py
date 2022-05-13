66f83c0bf846cf45b3fe972f2d18557fe4fb0e01
luizgodoy26 <luizgodoy26@gmail.com>
2022-05-13 16:44:58 -0300

    New adjustments on clients person froms

--- clients/forms.py
+++ clients/forms.py
@@ -24,6 +24,14 @@ class ClientCompanyForm(ModelForm):
 FORM FOR CLIENT PERSON
 """
 class ClientPersonForm(ModelForm):
+    first_name = forms.CharField(required=True, label='First name', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Client first name'}))
+    last_name = forms.CharField(required=True, label='Last name', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Client last name'}))
+    cpf = forms.IntegerField(required=True, label='CPF', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Client CPF'}))
+    phone = forms.IntegerField(required=False, label='Phone', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Client phone number'}))
+    email = forms.EmailField(required=False, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Company e-mail'}))
+    pending_payments = forms.DecimalField(required=False, label='Pending payments', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Pending payments from the client'}))
+    received_payments = forms.DecimalField(required=False, label='Received payments', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Received payments from the client'}))
+
     class Meta:
         model = ClientPerson
         fields = ['first_name','last_name', 'cpf', 'phone', 'email', 'person_company', 'pending_payments', 'received_payments', 'description']
