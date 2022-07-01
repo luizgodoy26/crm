b5ad6ecc1f2895ce4239f634d6be95ae92539ece
luizgodoy26 <luizgodoy26@gmail.com>
2022-07-01 19:25:00 -0300

    STYLE: Adjusted the style of the search field

--- clients/filters.py
+++ clients/filters.py
@@ -1,14 +1,25 @@
 import django_filters
+from django.forms import TextInput
 
 from accounts import forms
 from . import models
-from .models import ClientCompany
+from .models import ClientCompany, ClientPerson
+
 
 class CompanyFilter(django_filters.FilterSet):
-    company_name = django_filters.CharFilter(lookup_expr='icontains', label='Search by company name')
-    email = django_filters.CharFilter(lookup_expr='icontains', label='Search by email')
+    company_name = django_filters.CharFilter(lookup_expr='icontains', label='Search', widget=TextInput(attrs={'class': 'search right', 'placeholder': 'Company name'}))
 
 
     class Meta:
         model = ClientCompany
-        fields = ('company_name', 'email')
+        fields = ['company_name']
+
+
+class PersonFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains', label='Search by first name')
+    last_name = django_filters.CharFilter(lookup_expr='icontains', label='Search by last name')
+
+
+    class Meta:
+        model = ClientPerson
+        fields = ('first_name', 'last_name')
